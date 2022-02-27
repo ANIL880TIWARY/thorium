02204407cc23c67a88130bc7cc52a8c5741380d6
Anil Tiwary <kumar880911anil@gmail.com>
2022-02-26 23:13:40 -0800

    trying my 1st branch

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
+router.get('/students/:user', function (req, res) {
+    let studentName = req.params.user
     console.log(studentName)
     res.send(studentName)
 })
+//problem 1
+router.get('/movies', function (req, res) {
+    let mov = ["anil", "kumar", "tiwary", "dhanbad"]
+    res.send(mov)
+
+})
 
 
+
+//Problem 2
+router.get('/movies/:moviesid', function (req, res) {
+    let mov = ["anil", "kumar", "tiwary", "dhanbad"]
+    let value = req.params.moviesid;
+    if (value>mov.lenght -1) {
+         res.send("ERROR") 
+        }
+    else {
+         res.send(mov[value])
+         }
+
+})
+
+//problem 3
 
 
 module.exports = router;
