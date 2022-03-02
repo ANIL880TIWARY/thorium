a2836bf18e4ef853d4e49db3e946615937cf00a3
Anil Tiwary <kumar880911anil@gmail.com>
2022-03-03 01:03:59 +0530

    get api assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,7 +7,37 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+router.get('/students/:user', function (req, res) {
+    let studentName = req.params.user
+    console.log(studentName)
+    res.send(studentName)
+})
+//problem 1
+router.get('/movies', function (req, res) {
+    let mov = ["anil", "kumar", "tiwary", "dhanbad"]
+    res.send(mov)
 
+})
+
+
+
+//Problem 2
+router.get('/movies/:moviesid', function (req, res) {
+    let mov = ["anil", "kumar", "tiwary", "dhanbad"]
+    let value = req.params.moviesid;
+    if (value>mov.lenght -1) {
+         res.send("ERROR") 
+        }
+    else {
+         res.send(mov[value])
+         }
+
+})
+
+//problem 3
 
 
 module.exports = router;
+
+
+
